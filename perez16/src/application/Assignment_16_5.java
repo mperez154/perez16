4a9274d65ff39015705c62bf7f57f017f47a444f
mperez5 <marco.perez@RaymondJames.com>
2016-10-18 15:37:09 -0400

    Added code for Binary to Decimal method

--- perez16/src/application/Assignment_16_5.java
+++ perez16/src/application/Assignment_16_5.java
@@ -53,8 +53,17 @@ public class Assignment_16_5 extends Application {
 		
 		tfBinary.setOnAction(e -> 
 		{
-			//input.setText(input3.getText());
-			//input2.setText(input3.getText());
+			try
+			{
+				binaryToDecimal(tfBinary.getText().toString());		
+				tfDecimal.setText(myDecimal + "");
+				tfHex.setText(myHex);
+			}
+			catch(Exception e1)
+			{
+				System.out.println("Error occured with binary input/entry");
+			}
+			
 		});
 		
 
@@ -71,32 +80,40 @@ public class Assignment_16_5 extends Application {
 		myDecimal = Integer.parseInt(input, 10);	//String converted to Int
 		myHex = Integer.toHexString(myDecimal);	//Int to Hex
 		myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
-		//System.out.println(myDecimal + " " + myHex + " " + myBinary);
 	}
 	
 	public void hexToDecimal(String input)
 	{
 		
-		// Convert Hex to Decimal (from book sample on page 143		
-		char ch = input.toUpperCase().charAt(0);
-		if (ch <= 'F' && ch >= 'A') {
-			myDecimal = ch - 'A' + 10;
-			myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
-			System.out.println("The decimal value for hex digit " 	+ ch + " is " + myDecimal);
-		 }
-		 else if (Character.isDigit(ch)) {
-			 System.out.println("The decimal value for hex digit " + ch + " is " + ch);
-			 myDecimal = ch;
-			 myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
-		 }
-		 else {
-		 System.out.println(ch + " is an invalid input");
-		 }
-		
-		 
+		// Convert Hex to Decimal (from book sample on page 143)
+		if(input.toUpperCase().charAt(0) > 'F')
+		{
+			System.out.println("Invalid character entered");
+		}
+		else
+		{
+			char ch = input.toUpperCase().charAt(0);
+			if (ch <= 'F' && ch >= 'A') {
+				myDecimal = ch - 'A' + 10;
+				myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
+			 }
+			 else if (Character.isDigit(ch)) {
+				 myDecimal = ch;
+				 myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
+			 }
+			 else {
+			 System.out.println(ch + " is an invalid input");
+			 }
+		} 
 	}
 	
-	
+	public void binaryToDecimal(String input)
+	{
+		//Convert string to decimal, hex and binary
+		myDecimal = Integer.parseInt(input, 2);	//String converted to Int
+		myHex = Integer.toHexString(myDecimal);	//Int to Hex
+		myBinary = Integer.parseInt(input, 2) + "";
+	}
 	
 	
 	
