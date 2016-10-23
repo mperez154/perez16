82416858f08dd6497c2a60421c23a24dcfb03f59
Marco <mperez154@gmail.com>
2016-10-23 11:15:44 -0400

    Formatted with Java style braces, added comments

--- perez16/src/application/Assignment_16_13.java
+++ perez16/src/application/Assignment_16_13.java
@@ -21,78 +21,72 @@ public class Assignment_16_13 extends Application {
 	protected Text tfInterest = new Text(5, 20, "Interest Rate");
 	protected Text tfPayment = new Text(5, 20, "Monthly Payment");
 	protected Text tfTotal = new Text(5, 20, "Total Payment");
-	protected TextArea taResults =  new TextArea();
-	protected Double[] interestRate = {5.0, 5.25, 5.5, 5.75, 6.00, 6.25, 6.5, 6.75, 7.00, 7.25, 7.5, 7.75, 8.0};
-	
-	protected BorderPane getPane() {
-		
-		//Create variables and required fields
-	    HBox paneForInput = new HBox(20);
-	    Button btSubmit = new Button("Show Table"); 
-	    TextField tfAmount = new TextField();
-	    TextField tfYears = new TextField();
-	    Label lbAmount = new Label("Loan Amount");
-	    Label lbYears = new Label("Number of Years");
-	    
-	    //Initialize and create parameters for fields
-	    paneForInput.getChildren().addAll(lbAmount, tfAmount, lbYears, tfYears, btSubmit);
-	    paneForInput.setAlignment(Pos.CENTER_LEFT);
-	    paneForInput.setPadding(new Insets (5,5,5,5));
-	    paneForInput.setStyle("-fx-border-color: black");
-	    tfAmount.setMaxWidth(80);
-	    tfYears.setMaxWidth(45);
-	    tfInterest.setTextAlignment(TextAlignment.LEFT);
-	    tfPayment.setTextAlignment(TextAlignment.LEFT);
-	    tfTotal.setTextAlignment(TextAlignment.LEFT);
-	    
-	    //Create a main pane
-	    BorderPane pane = new BorderPane();
-	    pane.setTop(paneForInput);
-	    
-	    //Create a pane setLeft for the interest rate column
-	    Pane paneForResults = new Pane();
-	    //paneForResults.setMinWidth(150);
-	    paneForResults.getChildren().add(taResults);    	    
-	    pane.setLeft(paneForResults);
-	    
-	    //Show Table button setOnAction event
-	    btSubmit.setOnAction(e -> 
-	    {
-	    	taResults.setText(tfInterest.getText() + "\t\t" + tfPayment.getText() + "\t\t" + tfTotal.getText() + "\n");
-	    	
-	    	int years = Integer.parseInt(tfYears.getText().toString());
-	    	double amount = Double.parseDouble(tfAmount.getText().toString());
-	    	
-		    for(int i = 0; i < interestRate.length; i++)
-		    {
-		    	//Create instance of Loan class
-			    Loan myLoan = new Loan(interestRate[i].doubleValue(), years, amount);
-		    	//Append text for taResults text area
-			    taResults.appendText(interestRate[i].doubleValue() + "\t\t\t\t" + myLoan.getMonthlyPayment() + "\t" + myLoan.getTotalPayment() + "\n");
-			    i++;
-			    
-				System.out.printf("Payment is $%7.2f\n",(myLoan.getMonthlyPayment()));
-				
-				//NumberFormat test = myLoan.getMonthlyPayment().NumberFormat.getCurrencyInstance();
+	protected TextArea taResults = new TextArea();
+	protected Double[] interestRate = { 5.0, 5.125, 5.25, 5.375, 5.5, 5.625, 5.75, 5.875, 6.00, 6.125, 6.25, .375, 6.5,
+			6.625, 6.75, 6.875, 7.00, 7.125, 7.25, 7.375, 7.5, 7.625, 7.75, 7.875, 8.0 };
 
-		    	
-		    }
-		    
-		   
-	    }
-	    );
-	    
-	    return pane;
-	  }
+	protected BorderPane getPane() {
+
+		// Create variables and required fields
+		HBox paneForInput = new HBox(20);
+		Button btSubmit = new Button("Show Table");
+		TextField tfAmount = new TextField();
+		TextField tfYears = new TextField();
+		Label lbAmount = new Label("Loan Amount");
+		Label lbYears = new Label("Number of Years");
+
+		// Initialize and create parameters for fields
+		paneForInput.getChildren().addAll(lbAmount, tfAmount, lbYears, tfYears, btSubmit);
+		paneForInput.setAlignment(Pos.CENTER_LEFT);
+		paneForInput.setPadding(new Insets(5, 3, 5, 3));
+		paneForInput.setStyle("-fx-border-color: black");
+		tfAmount.setMaxWidth(60);
+		tfYears.setMaxWidth(45);
+		tfInterest.setTextAlignment(TextAlignment.LEFT);
+		tfPayment.setTextAlignment(TextAlignment.LEFT);
+		tfTotal.setTextAlignment(TextAlignment.LEFT);
+
+		// Create a main pane
+		BorderPane pane = new BorderPane();
+		pane.setTop(paneForInput);
+
+		// Create a pane setLeft for the interest rate column
+		Pane paneForResults = new Pane();
+		// paneForResults.setMinWidth(150);
+		paneForResults.getChildren().add(taResults);
+		pane.setLeft(paneForResults);
+
+		// Show Table button setOnAction event
+		btSubmit.setOnAction(e -> {
+			taResults.setText(tfInterest.getText() + "\t\t" + tfPayment.getText() + "\t\t" + tfTotal.getText() + "\n");
+
+			int years = Integer.parseInt(tfYears.getText().toString());
+			double amount = Double.parseDouble(tfAmount.getText().toString());
+
+			for (int i = 0; i < interestRate.length; i++) {
+				// Create instance of Loan class
+				Loan myLoan = new Loan(interestRate[i].doubleValue(), years, amount);
+				// Used append option and numberFormat class, information
+				// provided in docs.oracle.com site (see collaboration statement for more details
+				taResults.appendText(interestRate[i].doubleValue() + "\t\t\t\t"
+						+ NumberFormat.getCurrencyInstance().format(myLoan.getMonthlyPayment()) + "\t\t\t\t"
+						+ NumberFormat.getCurrencyInstance().format(myLoan.getTotalPayment()) + "\n");
+				i++;
+			}
+
+		});
+
+		return pane;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
 		// Create a scene and place it in the stage
-	    Scene scene = new Scene(getPane());
-	    primaryStage.setTitle("Assignment_16_13"); // Set the stage title
-	    primaryStage.setScene(scene); // Place the scene in the stage
-	    primaryStage.show(); // Display the stage
-		
+		Scene scene = new Scene(getPane());
+		primaryStage.setTitle("Assignment_16_13"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+
 	}
 
 	public static void main(String[] args) {
--- perez16/src/application/Assignment_16_5.java
+++ perez16/src/application/Assignment_16_5.java
@@ -21,102 +21,83 @@ public class Assignment_16_5 extends Application {
 		pane.setVgap(5.5);
 		pane.setHgap(10.0);
 
-		//Creating labels
-		pane.add(new Label("Decimal"), 0,0);
-		pane.add(new Label("Hex"), 0,1);
-		pane.add(new Label("Binary"), 0,2);
-		
-		//Creating TextFields
+		// Creating labels
+		pane.add(new Label("Decimal"), 0, 0);
+		pane.add(new Label("Hex"), 0, 1);
+		pane.add(new Label("Binary"), 0, 2);
+
+		// Creating TextFields
 		TextField tfDecimal = new TextField();
 		TextField tfHex = new TextField();
 		TextField tfBinary = new TextField();
-		
+
 		pane.add(tfDecimal, 1, 0);
 		pane.add(tfHex, 1, 1);
 		pane.add(tfBinary, 1, 2);
-		
-		
-		tfDecimal.setOnAction(e -> 
-		{
+
+		tfDecimal.setOnAction(e -> {
 			decimalMethod(tfDecimal.getText().toString());
 			tfHex.setText(myHex);
 			tfBinary.setText(myBinary);
-			
+
 		});
-		
-		tfHex.setOnAction(e -> 
-		{
-			hexToDecimal(tfHex.getText().toString());
+
+		tfHex.setOnAction(e -> {
+			hexToDecimal(tfHex.getText().toString().toUpperCase()); // adding to Upper b/c I noticed some hex to decimal conversions were incorrect when using lower case letters
 			tfDecimal.setText(myDecimal + "");
 			tfBinary.setText(myBinary);
 		});
-		
-		tfBinary.setOnAction(e -> 
-		{
-			try
-			{
-				binaryToDecimal(tfBinary.getText().toString());		
+
+		tfBinary.setOnAction(e -> {
+			try {
+				binaryToDecimal(tfBinary.getText().toString());
 				tfDecimal.setText(myDecimal + "");
 				tfHex.setText(myHex);
-			}
-			catch(Exception e1)
-			{
+			} catch (Exception e1) {
 				System.out.println("Error occured with binary input/entry");
 			}
-			
-		});
-		
 
-		Scene scene = new Scene(pane, 250, 200);
+		});
+
+		Scene scene = new Scene(pane, 275, 150);
 		primaryStage.setTitle("Assignment 16.5");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 	}
-	
-	public void decimalMethod(String input)
-	{
-		//Convert string to decimal, hex and binary
-		myDecimal = Integer.parseInt(input, 10);	//String converted to Int
-		myHex = Integer.toHexString(myDecimal);	//Int to Hex
-		myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
+
+	public void decimalMethod(String input) {
+		// Convert string to decimal, hex and binary conversions right after
+		myDecimal = Integer.parseInt(input, 10); // String converted to int
+		myHex = Integer.toHexString(myDecimal); // int to Hex
+		myBinary = Integer.toBinaryString(myDecimal); // int to Binary
 	}
-	
-	public void hexToDecimal(String input)
-	{
-		
-		// Convert Hex to Decimal (from book sample on page 143)
-		if(input.toUpperCase().charAt(0) > 'F')
-		{
-			System.out.println("Invalid character entered");
+
+	public void hexToDecimal(String hex) {
+		myDecimal = 0;
+		for (int i = 0; i < hex.length(); i++) {
+			char hexChar = hex.charAt(i);
+			myDecimal = myDecimal * 16 + hexCharToDecimal(hexChar);
+			myBinary = Integer.toBinaryString(myDecimal);
 		}
-		else
-		{
-			char ch = input.toUpperCase().charAt(0);
-			if (ch <= 'F' && ch >= 'A') {
-				myDecimal = ch - 'A' + 10;
-				myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
-			 }
-			 else if (Character.isDigit(ch)) {
-				 myDecimal = ch;
-				 myBinary = Integer.toBinaryString(myDecimal);	//Int to Binary
-			 }
-			 else {
-			 System.out.println(ch + " is an invalid input");
-			 }
-		} 
+
 	}
 	
-	public void binaryToDecimal(String input)
-	{
-		//Convert string to decimal, hex and binary
-		myDecimal = Integer.parseInt(input, 2);	//String converted to Int
-		myHex = Integer.toHexString(myDecimal);	//Int to Hex
+	//Next to methods were copied from samples on page 143 of the textbook
+	public static int hexCharToDecimal(char ch) {
+		if (ch >= 'A' && ch <= 'F')
+			return 10 + ch - 'A';
+		else // ch is '0', '1', ..., or '9'
+			return ch - '0';
+	}
+
+	public void binaryToDecimal(String input) {
+		// Convert string to decimal, hex and binary
+		myDecimal = Integer.parseInt(input, 2); // String converted to Int
+		myHex = Integer.toHexString(myDecimal); // Int to Hex
 		myBinary = Integer.parseInt(input, 2) + "";
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
